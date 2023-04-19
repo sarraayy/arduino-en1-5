9cfe156dd487db8473ecad8b989e05fd914f19b8
saray <saray.alvarado@my.utsa.edu>
2023-04-19 13:56:20 -0500

    saray commit

--- songcode/songcode.ino
+++ songcode/songcode.ino
@@ -5,9 +5,13 @@ void loop() {
   // Jacob codes here
 int A4 = 440.00; 
 int G4 = 392.00;
-int F4# = 369.99;
+int F4 = 369.99;
 int B4 = 493.88; 
 int sixteenth = 333;
 
   // Saray codes here
+int eighth = 288;
+int quarter = 577;
+int dottedeighth = 432.7;
+
 }

48a2f2d45f4ffb5b824c363822aac724926f4530
cjacob5 <129323644+cjacob5@users.noreply.github.com>
2023-04-19 13:20:04 -0500

    Variables added

--- songcode.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-}
-
-void loop() {
-  // Jacob codes here
-  int C4=262;
-
-  // Saray codes here
-}
---
+++ songcode/songcode.ino
@@ -0,0 +1,13 @@
+void setup() {
+}
+
+void loop() {
+  // Jacob codes here
+int A4 = 440.00; 
+int G4 = 392.00;
+int F4# = 369.99;
+int B4 = 493.88; 
+int sixteenth = 333;
+
+  // Saray codes here
+}

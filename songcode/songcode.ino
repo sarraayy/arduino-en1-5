fca86232d26d43664553e421ecf0a6f584985a91
cjacob5 <129323644+cjacob5@users.noreply.github.com>
2023-04-22 12:10:07 -0500

    New Song code

--- songcode/songcode.ino
+++ songcode/songcode.ino
@@ -2,16 +2,95 @@ void setup() {
 }
 
 void loop() {
-  // Jacob codes here
+   // Jacob codes here
+int output = 13;
 int A4 = 440.00; 
 int G4 = 392.00;
 int F4 = 369.99;
 int B4 = 493.88; 
 int sixteenth = 333;
 
-  // Saray codes here
+
+ // Saray codes here
 int eighth = 288;
 int quarter = 577;
 int dottedeighth = 432.7;
 
+  //Bar 1
+//A4
+delay(eighth);
+  tone(output, A4);
+  delay(eighth);
+noTone(output);
+  delay(50);
+//A4
+  tone(output, A4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//A4
+  tone(output, A4);
+  delay(sixteenth);
+  noTone(output);
+  delay(50);
+//A4
+  tone(output,A4);
+  delay(dottedeighth);
+  noTone(output);
+  delay(50);
+//G4
+  tone(output, G4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//F4
+  tone(output, F4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//G4
+  tone(output, G4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//A4
+ tone(output, A4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//A4
+ tone(output, A4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//A4
+  tone(output, A4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//B4
+  tone(output, B4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
+//A4
+   tone(output, A4);
+  delay(sixteenth);
+  noTone(output);
+  delay(50);
+//G4
+  tone(output, G4);
+  delay(dottedeighth);
+  noTone(output);
+  delay(50);
+//F4
+ tone(output, F4);
+ delay(eighth);
+ noTone(output);
+ delay(50);
+//G4
+tone(output, G4);
+  delay(eighth);
+  noTone(output);
+  delay(50);
 }
